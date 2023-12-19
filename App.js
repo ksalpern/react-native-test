e6f871c1d6435c6713fee816fc5cd6d41250a38f
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-12-19 19:13:15 +0200

    add post card

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from "react-native";
+import styled from "styled-components/native";
+
+const Post = styled.View`
+  flex-direction: row;
+  padding: 15px;
+  border-bottom-width: 1px;
+  border-bottom-color: #334339;
+  border-bottom-style: solid;
+`;
+
+const PostImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 12px;
+  margin-right: 12px;
+`;
+
+const PostDetails = styled.View`
+  justify-content: center;
+`;
+
+const PostTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const PostDate = styled.Text`
+  font-size: 14px;
+  color: #ccc;
+  margin-top: 4px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
       <StatusBar style="auto" />
+      <Post>
+        <PostImage
+          source={{
+            uri: "https://lh3.googleusercontent.com/ogw/ANLem4aS7uJ6Xuo9evWHZOSmpsAhc3hry300OtoqiJRqTxc=s64-c-mo",
+          }}
+        />
+        <PostDetails>
+          <PostTitle>React Native</PostTitle>
+          <PostDate>10/07/2023</PostDate>
+        </PostDetails>
+      </Post>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
